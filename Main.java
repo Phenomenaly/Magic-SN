c216d914adc2d9088aa9b0412513fc4c39755139
Im most Phenomenaly! <136328121+Phenomenaly@users.noreply.github.com>
2023-10-25 00:56:31 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,5 +20,23 @@ public class Main {
 
         System.out.println(Number.Division(newNum1, newNum2)); // 1.0043859649122806043664013486704789102077484130859375
         System.out.println(Number.Division(newNum2, newNum1)); // 0.FEE1D10C4C0478
+        
+        // Number newNum1 = new Number("10.A", 10);
+        // Number newNum2 = new Number("A.B", 16);
+        
+        // System.out.println(newNum1.dec); // NaN (double)
+        // System.out.println(newNum2.dec); // NaN (String)
+
+        // System.out.println(Number.Plus(newNum1, newNum2)); // NaN (double)
+        // System.out.println(Number.Plus(newNum2, newNum1)); // NaN (String)
+
+        // System.out.println(Number.Minus(newNum1, newNum2)); // NaN (double)
+        // System.out.println(Number.Minus(newNum2, newNum1)); // NaN (String)
+
+        // System.out.println(Number.Multiply(newNum1, newNum2)); // NaN (double)
+        // System.out.println(Number.Multiply(newNum2, newNum1)); // NaN (String)
+
+        // System.out.println(Number.Division(newNum1, newNum2)); // NaN (double)
+        // System.out.println(Number.Division(newNum2, newNum1)); // NaN (String)           
     }
 }
